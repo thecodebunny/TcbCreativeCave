8d183ab379617bbefa822a982c45bfed1ba55480
TheCodeBunny <thecodebunny@gmail.com>
2022-05-19 15:39:14 +0200

    Initial commit

---
+++ src/TcbCreativeCave.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace TcbCreativeCave;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class TcbCreativeCave extends Plugin implements ThemeInterface
+{
+}
