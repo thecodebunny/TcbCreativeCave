28407bfd8bdc827467272a83a61f96c2a914eee2
TheCodeBunny <thecodebunny@gmail.com>
2022-05-20 11:11:18 +0200

    Creating first CMS Block in Shopware 6 Administration

---
+++ src/Resources/app/administration/src/module/sw-cms/blocks/text-image/tcb-initial-image/preview/index.js
@@ -0,0 +1,6 @@
+import template from './sw-cms-preview-tcb-initial-image.html.twig';
+import './sw-cms-preview-tcb-initial-image.scss';
+
+Shopware.Component.register('sw-cms-preview-tcb-initial-image', {
+    template
+});
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v1.0.0',
+        'version' => '1.0.0.0',
+        'type' => 'shopware-platform-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'thecodebunny/tcb-creative-cave',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'thecodebunny/tcb-creative-cave' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'shopware-platform-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
